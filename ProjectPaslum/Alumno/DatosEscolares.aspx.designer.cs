204dfafa5212110facd800a4a86b12856895aa46
Rodrigo Montero Orozco <rodrigo.montero1909@gmail.com>
2019-09-19 19:37:10 -0500

    [Alm] Datos escolares (Norma)

--- ProjectPaslum/Alumno/DatosEscolares.aspx.designer.cs
+++ ProjectPaslum/Alumno/DatosEscolares.aspx.designer.cs
@@ -11,5 +11,59 @@ namespace ProjectPaslum.Alumno {
     
     
     public partial class DatosEscolares {
+        
+        /// <summary>
+        /// Control LabNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabNombre;
+        
+        /// <summary>
+        /// Control DropMateria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropMateria;
+        
+        /// <summary>
+        /// Control LabCuatri.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabCuatri;
+        
+        /// <summary>
+        /// Control DropCuatrimestre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropCuatrimestre;
+        
+        /// <summary>
+        /// Control LabCarrera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabCarrera;
+        
+        /// <summary>
+        /// Control DropCarrera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropCarrera;
     }
 }
